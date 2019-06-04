6f7993c69e9c1bdde5d4e2ad8b436ad8cb52d01a
Tech-A06 <tech-a06@Tech-A06s-MacBook-Air.local>
2019-06-04 11:53:08 +0100

    removed buggy test

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -7,6 +7,8 @@
 require 'spec_helper'
 
 describe 'python::default' do
+  ChefSpec::Coverage.start!
+
   context 'When all attributes are default, on Ubuntu 16.04' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
@@ -33,19 +35,13 @@ describe 'python::default' do
     it 'should upgrade python-pip' do
       expect(chef_run).to upgrade_package('python-pip')
     end
-
-   it 'should enable pip install' do
-     expect(chef_run).to enable_service "pip install"
-   end
-   it 'should start pip install' do
-    expect(chef_run).to start_service "pip install"
-  end
   it 'should create a requirements.txt template in /etc/python3/' do
     expect(chef_run).to create_template("/etc/python3/requirements.txt")
   end
   it 'should execute pip install' do
     expect(chef_run).to run_execute('pip install')
   end
-    at_exit { ChefSpec::Coverage.report! }
   end
+  at_exit { ChefSpec::Coverage.report! }
+
 end
